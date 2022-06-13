6d3c4ce7c9221f36fea82def3df2c968f86f1e9f
Marcus Vinícius <marcusvinici25@gmail.com>
2022-06-13 18:52:09 -0300

    creating functions for graphql intercepts

--- tests/e2e/pages/helpers/handleEqualIntercepts.js
+++ tests/e2e/pages/helpers/handleEqualIntercepts.js
@@ -1,132 +1,14 @@
 import {
   UserBranchsData,
   Forksdata,
-  ReposUserData,
-  StatusPrGithubData,
-  PrCommitsGithubData,
 } from "../payload/bounty";
 
 import { userData } from "../payload/user";
 
-const hostGithub = "api.github.com";
+import { hasQueryName } from "../../utils/graphql-test-utils";
+
 
 export const interceptGroup = {
-  getGithubForks: () =>
-    cy
-      .intercept(
-        {
-          method: "GET",
-          url: "/repos/*/*/forks?per_page=100",
-          hostname: hostGithub,
-        },
-        {
-          statusCode: 200,
-          body: Forksdata,
-        }
-      )
-      .as("getGithubForks"),
-
-  getGithubCurrentBranchs: () =>
-    cy
-      .intercept(
-        {
-          method: "GET",
-          url: "/repos/*/*/branches",
-          hostname: hostGithub,
-        },
-        UserBranchsData
-      )
-      .as("getGithubCurrentBranchs"),
-
-  getGithubCurrentRepos: () =>
-    cy
-      .intercept(
-        {
-          method: "GET",
-          url: "/repos/*/*",
-          hostname: hostGithub,
-        },
-        ReposUserData
-      )
-      .as("getGithubCurrentRepos"),
-
-  getIssueCommentsGithub: () =>
-    cy
-      .intercept(
-        {
-          method: "GET",
-          url: "/repos/*/*/issues/*/comments",
-          hostname: hostGithub,
-        },
-        {
-          statusCode: 200,
-          body: [],
-        }
-      )
-      .as("getIssueCommentsGithub"),
-
-  postPullRequest: () =>
-    cy
-      .intercept(
-        {
-          method: "POST",
-          url: "/api/pull-request",
-          times: 2000,
-        },
-        {
-          body: "ok",
-        }
-      )
-      .as("postPullRequest"),
-
-  getPrCount: () =>
-    cy
-      .intercept("GET", "/api/pull-request?*", {
-        count: 1,
-      })
-      .as("getPrCount"),
-
-  getPrGithub: () =>
-    cy
-      .intercept(
-        {
-          method: "GET",
-          url: "/repos/*/*/pulls/*",
-          hostname: "api.github.com",
-        },
-        {
-          body: StatusPrGithubData,
-        }
-      )
-      .as("getPrGithub"),
-
-  getParticipants: () =>
-    cy
-      .intercept(
-        {
-          method: "GET",
-          url: "/repos/*/*/pulls/*/commits",
-          hostname: "api.github.com",
-        },
-        {
-          body: [PrCommitsGithubData],
-        }
-      )
-      .as("getParticipants"),
-
-  getCommits: () =>
-    cy
-      .intercept(
-        {
-          method: "GET",
-          url: "/repos/*/*/commits",
-          hostname: "api.github.com",
-        },
-        {
-          body: [PrCommitsGithubData],
-        }
-      )
-      .as("getCommits"),
 
   searchUsers: (address) =>
     cy
@@ -143,4 +25,141 @@ export const interceptGroup = {
         body: [userData(address)],
       })
       .as("getAddress"),
+
+  gqlForksQuery: (req) => {
+    if (hasQueryName(req, "query Forks")) {
+      req.alias = "gqlForksQuery";
+      req.reply(({ body }) => {
+        body.data ? (body.data = null) : null;
+        body.data = Forksdata;
+        return body;
+      });
+    }
+  },
+
+  gqlBranchesQuery: (req) => {
+    if (hasQueryName(req, "query Branches")) {
+      req.alias = "gqlBranchesQuery";
+      req.reply(({ body }) => {
+        body.data ? (body.data = null) : null;
+        body.data = UserBranchsData;
+        return body;
+      });
+    }
+  },
+
+  gqlRepositoriesQuery: (req) => {
+    if (hasQueryName(req, "query Repositories")) {
+      req.alias = "gqlRepositoriesQuery";
+      req.reply(({ body }) => {
+        body.data ? (body.data = null) : null;
+        body.data = {
+          user: {
+            repositories: {
+              pageInfo: {
+                endCursor: "Y3Vyc29yOnYyOpHOG9_rrw==",
+                hasNextPage: false,
+              },
+              nodes: [
+                {
+                  name: "bepro-js-edge",
+                  nameWithOwner: "user-test/bepro-js-edge",
+                  isFork: true,
+                  owner: {
+                    login: "user-test",
+                  },
+                },
+              ],
+            },
+            organizations: {
+              nodes: [],
+            },
+          },
+        };
+        return body;
+      });
+    }
+  },
+
+  gqlCommentsQuery: (req) => {
+    if (hasQueryName(req, "query Comments")) {
+      req.alias = "gqlCommentsQuery";
+      req.reply(({ body }) => {
+        body.data ? (body.data = null) : null;
+        body.data = {
+          repository: {
+            issueOrPullRequest: {
+              comments: {
+                pageInfo: {
+                  endCursor: null,
+                  hasNextPage: false,
+                },
+                nodes: [],
+              },
+            },
+          },
+        };
+        return body;
+      });
+    }
+  },
+
+  gqlDetailsQuery: (req) => {
+    if (hasQueryName(req, "query Details")) {
+      req.alias = "gqlDetailsQuery";
+      req.reply({
+        data: {
+          repository: {
+            pullRequest: {
+              id: "PR_kwDOGEZRks45NP5H",
+              mergeable: "MERGEABLE",
+              merged: false,
+              state: "OPEN",
+            },
+          },
+        },
+      });
+    }
+  },
+
+  gqlParticipantsQuery: (req) => {
+    if (hasQueryName(req, "query Participants")) {
+      req.alias = "gqlParticipantsQuery";
+      req.reply({
+        data: {
+          repository: {
+            pullRequest: {
+              participants: {
+                pageInfo: {
+                  endCursor: "Mg",
+                  hasNextPage: false,
+                },
+                nodes: [
+                  {
+                    login: "user-test",
+                  },
+                ],
+              },
+            },
+          },
+        },
+      });
+    }
+  },
+
+  gqlLinesOfCodeQuery: (req) => {
+    if (hasQueryName(req, "query LinesOfCode")) {
+      req.alias = "gqlLinesOfCodeQuery";
+      req.reply({
+        data: {
+          repository: {
+            pullRequest: {
+              additions: 2,
+              deletions: 1,
+            },
+          },
+        },
+      });
+    }
+  },
 };
