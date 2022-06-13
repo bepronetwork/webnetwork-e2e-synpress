fe430a9b3ee98159ea5ad0c56ba7372a7828f321
Marcus Vinícius <marcusvinici25@gmail.com>
2022-06-13 18:47:24 -0300

    adjusting prepare .env

--- prepare.js
+++ prepare.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 const keys = require("./keys.json");
 const path = require("node:path");
+require("dotenv").config();
 
 const address = Object.values(keys.private_keys)[0];
+const HOSTNAME = process.env.HOSTNAME
 
 const main = () => {
   if (fs.readFileSync(path.resolve(__dirname, "SYNPRESS_PRIVATEKEY")))
@@ -17,7 +19,8 @@ const main = () => {
     fs.unlinkSync(path.resolve(__dirname, ".env"));
   fs.writeFileSync(
     path.resolve(__dirname, ".env"),
-    `PRIVATE_KEY=${address}`,
+    `PRIVATE_KEY=${address}
+    HOSTNAME=${HOSTNAME}`,
     "utf-8"
   );
 };
